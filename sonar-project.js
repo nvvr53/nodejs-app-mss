caa43eddc92f1701aced367825d26905c81e8831
nvvr53 <nvvr53@gmail.com>
2022-06-16 07:52:17 +0000

    SonarUrl

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.110.209.118:9000/',
+  serverUrl: 'http://13.126.186.199:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
-	    'sonar.projectName': 'Node JS Application - Sample',
-	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': '84283ade512912faf58ed9e1afe6e58b9a26fee5',
+	    'sonar.projectName': 'Sample',
+	    'sonar.projectKey':'Sample',
+	    'sonar.login': '02feb0f62a50877bbefbbfe9b58f697067328340',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
